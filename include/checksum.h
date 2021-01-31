f0ca9ce5129b402456b5920a596166842f2c0a09
Jon Daniel <jopadan@ctemplar.com>
2021-01-31 21:12:35 +0100

    Add CRC-CCITT32 to tests

--- include/checksum.h
+++ include/checksum.h
@@ -78,29 +78,29 @@
  * Prototype list of global functions
  */
 
-unsigned char *		checksum_NMEA(      const unsigned char *input_str, unsigned char *result  );
-uint8_t			crc_8(              const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_16(             const unsigned char *input_str, size_t num_bytes       );
-uint32_t		crc_32(             const unsigned char *input_str, size_t num_bytes       );
-uint64_t		crc_64_ecma(        const unsigned char *input_str, size_t num_bytes       );
-uint64_t		crc_64_we(          const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_ccitt_1d0f(     const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_ccitt_ffff(     const unsigned char *input_str, size_t num_bytes       );
-uint32_t		crc_ccitt32_ffffffff(     const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_dnp(            const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_kermit(         const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_modbus(         const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_sick(           const unsigned char *input_str, size_t num_bytes       );
-uint16_t		crc_xmodem(         const unsigned char *input_str, size_t num_bytes       );
-uint8_t			update_crc_8(       uint8_t  crc, unsigned char c                          );
-uint16_t		update_crc_16(      uint16_t crc, unsigned char c                          );
-uint32_t		update_crc_32(      uint32_t crc, unsigned char c                          );
-uint64_t		update_crc_64_ecma( uint64_t crc, unsigned char c                          );
-uint16_t		update_crc_ccitt(   uint16_t crc, unsigned char c                          );
+unsigned char *		checksum_NMEA(        const unsigned char *input_str, unsigned char *result  );
+uint8_t			crc_8(                const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_16(               const unsigned char *input_str, size_t num_bytes       );
+uint32_t		crc_32(               const unsigned char *input_str, size_t num_bytes       );
+uint64_t		crc_64_ecma(          const unsigned char *input_str, size_t num_bytes       );
+uint64_t		crc_64_we(            const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_ccitt_1d0f(       const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_ccitt_ffff(       const unsigned char *input_str, size_t num_bytes       );
+uint32_t		crc_ccitt32_ffffffff( const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_dnp(              const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_kermit(           const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_modbus(           const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_sick(             const unsigned char *input_str, size_t num_bytes       );
+uint16_t		crc_xmodem(           const unsigned char *input_str, size_t num_bytes       );
+uint8_t			update_crc_8(         uint8_t  crc, unsigned char c                          );
+uint16_t		update_crc_16(        uint16_t crc, unsigned char c                          );
+uint32_t		update_crc_32(        uint32_t crc, unsigned char c                          );
+uint64_t		update_crc_64_ecma(   uint64_t crc, unsigned char c                          );
+uint16_t		update_crc_ccitt(     uint16_t crc, unsigned char c                          );
 uint32_t		update_crc_ccitt32(   uint32_t crc, unsigned char c                          );
-uint16_t		update_crc_dnp(     uint16_t crc, unsigned char c                          );
-uint16_t		update_crc_kermit(  uint16_t crc, unsigned char c                          );
-uint16_t		update_crc_sick(    uint16_t crc, unsigned char c, unsigned char prev_byte );
+uint16_t		update_crc_dnp(       uint16_t crc, unsigned char c                          );
+uint16_t		update_crc_kermit(    uint16_t crc, unsigned char c                          );
+uint16_t		update_crc_sick(      uint16_t crc, unsigned char c, unsigned char prev_byte );
 
 /*
  * Global CRC lookup tables
--- test/testcrc.c
+++ test/testcrc.c
@@ -41,30 +41,32 @@
 
 #include "../include/checksum.h"
 
-						/************************************************/
-struct chk_tp {					/*						*/
-	const char *	input;			/* The input string to be checked		*/
-	uint8_t		crc8;			/* The  8 bit wide CRC8 of the input string	*/
-	uint16_t	crc16;			/* The 16 bit wide CRC16 of the input string	*/
-	uint32_t	crc32;			/* The 32 bit wide CRC32 of the input string	*/
-	uint64_t	crc64_ecma;		/* The 64 bit wide CRC64-ECMA of the input	*/
-	uint64_t	crc64_we;		/* The 64 bit wide CRC64-WE of the input string	*/
-	uint16_t	crcdnp;			/* The 16 bit wide DNP CRC of the string	*/
-	uint16_t	crcmodbus;		/* The 16 bit wide Modbus CRC of the string	*/
-	uint16_t	crcsick;		/* The 16 bit wide Sick CRC of the string	*/
-	uint16_t	crcxmodem;		/* The 16 bit wide XModem CRC of the string	*/
-	uint16_t	crc1d0f;		/* The 16 bit wide CCITT CRC with 1D0F start	*/
-	uint16_t	crcffff;		/* The 16 bit wide CCITT CRC with FFFF start	*/
-	uint16_t	crckermit;		/* The 16 bit wide CRC Kermit of the string	*/
-};						/*						*/
-						/************************************************/
+						/***************************************************/
+struct chk_tp {					/*						   */
+	const char *	input;			/* The input string to be checked		   */
+	uint8_t		crc8;			/* The  8 bit wide CRC8 of the input string	   */
+	uint16_t	crc16;			/* The 16 bit wide CRC16 of the input string	   */
+	uint32_t	crc32;			/* The 32 bit wide CRC32 of the input string	   */
+	uint64_t	crc64_ecma;		/* The 64 bit wide CRC64-ECMA of the input	   */
+	uint64_t	crc64_we;		/* The 64 bit wide CRC64-WE of the input string	   */
+	uint16_t	crcdnp;			/* The 16 bit wide DNP CRC of the string	   */
+	uint16_t	crcmodbus;		/* The 16 bit wide Modbus CRC of the string	   */
+	uint16_t	crcsick;		/* The 16 bit wide Sick CRC of the string	   */
+	uint16_t	crcxmodem;		/* The 16 bit wide XModem CRC of the string   	   */
+	uint16_t	crc1d0f;		/* The 16 bit wide CCITT CRC with 1D0F start	   */
+	uint16_t	crcffff;		/* The 16 bit wide CCITT CRC with FFFF start	   */
+	uint32_t        crcccitt32ffffffff;	/* The 32 bit wide CCITT32 CRC with FFFFFFFF start */
+	uint16_t	crckermit;		/* The 16 bit wide CRC Kermit of the string	   */
+};						/*						   */
+						/***************************************************/
 
 static struct chk_tp checks[] = {
-	{ "123456789",    0xA2, 0xBB3D, 0xCBF43926ul, 0x6C40DF5F0B497347ull, 0x62EC59E3F1A4F00Aull, 0x82EA, 0x4B37, 0x56A6, 0x31C3, 0xE5CC, 0x29B1, 0x8921 },
-	{ "Lammert Bies", 0xA5, 0xB638, 0x43C04CA6ul, 0xF806F4F5C0F3257Cull, 0xFE25A9F50630F789ull, 0x4583, 0xB45C, 0x1108, 0xCEC8, 0x67A2, 0x4A31, 0xF80D },
-	{ "",             0x00, 0x0000, 0x00000000ul, 0x0000000000000000ull, 0x0000000000000000ull, 0xFFFF, 0xFFFF, 0x0000, 0x0000, 0x1D0F, 0xFFFF, 0x0000 },
-	{ " ",            0x86, 0xD801, 0xE96CCF45ul, 0xCC7AF1FF21C30BDEull, 0x568617D9EF46BE26ull, 0x50D6, 0x98BE, 0x2000, 0x2462, 0xE8FE, 0xC592, 0x0221 },
-	{ NULL,           0,    0,      0,            0,                     0,                     0,      0,      0,      0,      0,      0,      0      }
+	{ "123456789",    0xA2, 0xBB3D, 0xCBF43926ul,  0x6C40DF5F0B497347ull, 0x62EC59E3F1A4F00Aull, 0x82EA, 0x4B37, 0x56A6, 0x31C3, 0xE5CC, 0x29B1, 0x0376E6E7ul, 0x8921 },
+	
+	{ "Lammert Bies", 0xA5, 0xB638, 0x43C04CA6ul, 0xF806F4F5C0F3257Cull, 0xFE25A9F50630F789ull, 0x4583, 0xB45C, 0x1108, 0xCEC8, 0x67A2, 0x4A31, 0xDC7513E5ul, 0xF80D },
+	{ "",             0x00, 0x0000, 0x00000000ul, 0x0000000000000000ull, 0x0000000000000000ull, 0xFFFF, 0xFFFF, 0x0000, 0x0000, 0x1D0F, 0xFFFF, 0xFFFFFFFFul, 0x0000 },
+	{ " ",            0x86, 0xD801, 0xE96CCF45ul, 0xCC7AF1FF21C30BDEull, 0x568617D9EF46BE26ull, 0x50D6, 0x98BE, 0x2000, 0x2462, 0xE8FE, 0xC592, 0xD62B0954, 0x0221 },
+	{ NULL,           0,    0,      0,            0,                     0,                     0,      0,      0,      0,      0,      0,      0,      0 }
 };
 
 /*
@@ -89,6 +91,7 @@ int test_crc( bool verbose ) {
 	uint16_t crc1d0f;
 	uint16_t crcffff;
 	uint16_t crckermit;
+	uint32_t crcccitt32ffffffff;
 	uint32_t crc32;
 	uint64_t crc64_ecma;
 	uint64_t crc64_we;
@@ -103,18 +106,19 @@ int test_crc( bool verbose ) {
 		ptr = (const unsigned char *) checks[a].input;
 		len = strlen( checks[a].input );
 
-		crc8       = crc_8(          ptr, len );
-		crc16      = crc_16(         ptr, len );
-		crc32      = crc_32(         ptr, len );
-		crc64_ecma = crc_64_ecma(    ptr, len );
-		crc64_we   = crc_64_we(      ptr, len );
-		crcdnp     = crc_dnp(        ptr, len );
-		crcmodbus  = crc_modbus(     ptr, len );
-		crcsick    = crc_sick(       ptr, len );
-		crcxmodem  = crc_xmodem(     ptr, len );
-		crc1d0f    = crc_ccitt_1d0f( ptr, len );
-		crcffff    = crc_ccitt_ffff( ptr, len );
-		crckermit  = crc_kermit(     ptr, len );
+		crc8               = crc_8(                ptr, len );
+		crc16              = crc_16(               ptr, len );
+		crc32              = crc_32(               ptr, len );
+		crcccitt32ffffffff = crc_ccitt32_ffffffff( ptr, len );
+		crc64_ecma         = crc_64_ecma(          ptr, len );
+		crc64_we           = crc_64_we(            ptr, len );
+		crcdnp             = crc_dnp(              ptr, len );
+		crcmodbus          = crc_modbus(           ptr, len );
+		crcsick            = crc_sick(             ptr, len );
+		crcxmodem          = crc_xmodem(           ptr, len );
+		crc1d0f            = crc_ccitt_1d0f(       ptr, len );
+		crcffff            = crc_ccitt_ffff(       ptr, len );
+		crckermit          = crc_kermit(           ptr, len );
 
 		if ( crc8 != checks[a].crc8 ) {
 
@@ -134,6 +138,13 @@ int test_crc( bool verbose ) {
 
 			if ( verbose ) printf( "\n    FAIL: CRC32 \"%s\" returns 0x%08" PRIX32 ", not 0x%08" PRIX32
 							, checks[a].input, crc32, checks[a].crc32 );
+			errors++;
+		}
+
+		if ( crcccitt32ffffffff != checks[a].crcccitt32ffffffff ) {
+
+			if ( verbose ) printf( "\n    FAIL: CRC CCITT32 ffffffff\"%s\" returns 0x%08" PRIX32 ", not 0x%08" PRIX32
+							, checks[a].input, crcccitt32ffffffff, checks[a].crcccitt32ffffffff );
 			errors++;
 		}
 
